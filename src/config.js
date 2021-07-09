0d56408846a5d372b768a24c8c088f5f56e441fe
Alex <dafen@ukr.net>
2021-07-09 10:55:34 +0300

    change token

--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
 module.exports = {
-    TOKEN: '1818383467:AAE0o9-CnjnUF8K2zV2ITz2mrSSJC4qDLsI',
+    TOKEN: '1860414673:AAFo5kV5d2bZQgVSUuSUpE9UgAqfsCcqcrI',
     DB_URL: 'mongodb://localhost/wfmcinema'
 }
 
 
+//new token
+//1860414673:AAFo5kV5d2bZQgVSUuSUpE9UgAqfsCcqcrI
 
 
 
