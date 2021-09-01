ba95d569f30feed4c9cb4f87e0b449702747e1e9
Alex <dafen@ukr.net>
2021-09-01 16:27:16 +0300

    begin create function for screen-calc

--- src/helper.js
+++ src/helper.js
@@ -14,6 +14,55 @@ module.exports = {
 
 }
 
+//=======================================================================================================
+
+
+const handler = (msg, aspectRatio) => {
+    const input = Number(msg.text)                
+    const sideFromInput = Math.round(input / aspectRatio)  
+    const diagonal = Math.round(Math.sqrt(Math.pow(input, 2) + Math.pow(sideFromInput, 2)))
+    const inputInInches = Math.round(input / 2.54)
+    const sideFromInputInInches = Math.round(sideFromInput / 2.54)
+    const diagonalInInches = Math.round(diagonal / 2.54)
+    const answer = `${input} x ${sideFromInput} см - ширина и высота экрана, формат 16:9
+                    \n${diagonal} см - диагональ экрана
+                    \n${inputInInches} x ${sideFromInputInInches} дюймов - ширина и высота экрана, формат 16:9
+                    \n${diagonalInInches} дюймов - диагональ экрана`                             
+                    
+    if (input && input > 0) {
+        bot.sendMessage(chatId, answer)                         
+    } 
+    else {           
+        bot.removeListener('message', handler);
+    }
+}
+
+
+
+
+/*
+
+function loco (msg, aspectRatio) {
+    const input = msg;                
+    const sideFromInput = Math.round(input / aspectRatio)  ;
+    const diagonal = Math.round(Math.sqrt(Math.pow(input, 2) + Math.pow(sideFromInput, 2)));
+    const inputInInches = Math.round(input / 2.54);
+    const sideFromInputInInches = Math.round(sideFromInput / 2.54);
+    const diagonalInInches = Math.round(diagonal / 2.54);
+    const answer = `${input} x ${sideFromInput} см - ширина и высота экрана, формат 16:9
+                    \n${diagonal} см - диагональ экрана
+                    \n${inputInInches} x ${sideFromInputInInches} дюймов - ширина и высота экрана, формат 16:9
+                    \n${diagonalInInches} дюймов - диагональ экрана`   ;                          
+                    
+   return answer
+}
+
+loco (333, 1.77777)
+
+
+*/
+
+
 
 
 
