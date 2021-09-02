39067735e644c3e8e7b9f116cb1532a001fccd35
Alex <dafen@ukr.net>
2021-09-02 12:14:53 +0300

    add diagonal clac to func screenCalculation

--- src/helper.js
+++ src/helper.js
@@ -42,25 +42,59 @@ const handler = (msg, aspectRatio) => {
 
 /*
 
-function loco (msg, aspectRatio) {
-    const input = msg;                
-    const sideFromInput = Math.round(input / aspectRatio)  ;
-    const diagonal = Math.round(Math.sqrt(Math.pow(input, 2) + Math.pow(sideFromInput, 2)));
-    const inputInInches = Math.round(input / 2.54);
-    const sideFromInputInInches = Math.round(sideFromInput / 2.54);
-    const diagonalInInches = Math.round(diagonal / 2.54);
-    const answer = `${input} x ${sideFromInput} см - ширина и высота экрана, формат 16:9
-                    \n${diagonal} см - диагональ экрана
-                    \n${inputInInches} x ${sideFromInputInInches} дюймов - ширина и высота экрана, формат 16:9
-                    \n${diagonalInInches} дюймов - диагональ экрана`   ;                          
-                    
-   return answer
+function screenCalculation (msg, aspectRatio, howSideInput) {
+    const input = msg;    
+    
+    if (howSideInput === 'width') {
+
+        const sideFromInput = Math.round(input / aspectRatio) 
+        const diagonal = Math.round(Math.sqrt(Math.pow(input, 2) + Math.pow(sideFromInput, 2)))
+        const inputInInches = Math.round(input / 2.54)
+        const sideFromInputInInches = Math.round(sideFromInput / 2.54)
+        const diagonalInInches = Math.round(diagonal / 2.54)
+        const answer = `${input} x ${sideFromInput} см - ширина и высота экрана, формат 16:9
+                        \n${diagonal} см - диагональ экрана
+                        \n${inputInInches} x ${sideFromInputInInches} дюймов - ширина и высота экрана, формат 16:9
+                        \n${diagonalInInches} дюймов - диагональ экрана`                                              
+        return answer
+    } else if (howSideInput === 'height') {
+        const sideFromInput = Math.round(input * aspectRatio)  
+        const diagonal = Math.round(Math.sqrt(Math.pow(input, 2) + Math.pow(sideFromInput, 2)))
+        const inputInInches = Math.round(input / 2.54)
+        const sideFromInputInInches = Math.round(sideFromInput / 2.54)
+        const diagonalInInches = Math.round(diagonal / 2.54)
+        const answer = `${sideFromInput} x ${input} см - ширина и высота экрана, формат 16:9
+                        \n${diagonal} см - диагональ экрана
+                        \n${sideFromInputInInches} x ${inputInInches} дюймов - ширина и высота экрана, формат 16:9
+                        \n${diagonalInInches} дюймов - диагональ экрана`                                                
+        return answer
+    } else if (howSideInput === 'diagonalInput') {
+
+        const widthFromInput = Math.round(Math.sqrt(Math.pow(input, 2) * Math.pow(aspectRatio, 2) / 2))
+        const heigtFromWidth = Math.round(input / aspectRatio) 
+        
+        const diagonalInInches = Math.round(input / 2.54)
+        const widthFromInputInInches = Math.round(widthFromInput / 2.54)
+        const heigtFromWidthInInches = Math.round(heigtFromWidth / 2.54)
+
+        const answer = `${widthFromInput} x ${heigtFromWidth} см - ширина и высота экрана, формат 16:9
+                        \n${input} см - диагональ экрана
+                        \n${widthFromInputInInches} x ${heigtFromWidthInInches} дюймов - ширина и высота экрана, формат 16:9
+                        \n${diagonalInInches} дюймов - диагональ экрана`                                                
+        return answer
+
+
+    } else {
+        console.log('Введите одно из трех значений: width или height, или diagonalInput')
+    }
+
 }
 
-loco (333, 1.77777)
-
-
+screenCalculation (381, 1.77777, 'diagonalInput')
 */
+
+
+
 
 
 
