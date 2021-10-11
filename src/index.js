97bd487d9ecee8faa52804e4e93f9a88fd0c691b
Alex <dafen@ukr.net>
2021-10-11 09:45:41 +0300

    add Date to Rate

--- src/index.js
+++ src/index.js
@@ -42,7 +42,9 @@ bot.on('message', msg => {
 
     switch (msg.text) {
         case kb.home.rate:
-            bot.sendMessage(chatId, config.EXCHANGE_RATE)
+            //bot.sendMessage(chatId, config.EXCHANGE_RATE)
+            const now = new Date().toLocaleString()
+            bot.sendMessage(chatId, now)
             break
         case kb.home.price:
             bot.sendMessage(chatId, `Ссылка для скачивания прайса\n 
